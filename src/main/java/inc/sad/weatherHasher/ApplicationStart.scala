f07c9f2fa14f79f9ab623e80135841dc2fd73246
Andrei Sadovskii <and.sadovsckij@yandex.ru>
2020-03-25 19:01:47 +0400

    Processing added

--- src/main/java/inc/sad/weatherHasher/ApplicationStart.scala
+++ src/main/java/inc/sad/weatherHasher/ApplicationStart.scala
@@ -1,7 +1,8 @@
 package inc.sad.weatherHasher
 
-import java.util.{Properties, UUID}
+import java.util.Properties
 
+import ch.hsr.geohash.GeoHash
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.kstream.{Consumed, Produced}
@@ -9,6 +10,11 @@ import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder, StreamsConfig}
 import org.apache.log4j.Logger
 import pureconfig.ConfigSource
 import pureconfig.generic.auto._
+
+/**
+ * It's only method for streaming processing weather data
+ * This method calculates geohash bases on longitude and latitude and sends it into kafka output topic
+ */
 
 object ApplicationStart extends App {
 
@@ -31,7 +37,9 @@ object ApplicationStart extends App {
     )
     .peek((key: String, value: String) => {
       try {
-        LOG.info(value)
+        val record = List.from(value.split(","))
+        val geoHash = GeoHash.geoHashStringWithCharacterPrecision(record(0).toDouble, record(2).toDouble, conf.applicationConfig.geohashPrecision)
+        record::List(geoHash)
       } catch {
         case e: Exception =>
           LOG.error(e)
--- src/main/java/inc/sad/weatherHasher/Config.scala
+++ src/main/java/inc/sad/weatherHasher/Config.scala
@@ -1,8 +1,13 @@
 package inc.sad.weatherHasher
 
+/**
+ *  A few case classes for describing config structure for PureConfig
+ */
+
 case class Config(applicationConfig: ApplicationConfig, kafkaConfig: KafkaConfig)
 
-case class ApplicationConfig(consumerPoolSize: Int)
+case class ApplicationConfig(geohashPrecision: Int,
+                             consumerPoolSize: Int)
 
 case class KafkaConfig(sourceTopic: String,
                        targetTopic: String,
